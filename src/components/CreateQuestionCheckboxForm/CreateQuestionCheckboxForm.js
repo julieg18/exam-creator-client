b0cabfd00d2fac38e68b0dcf8d818929b75382e5
julieg18 <juliannagal18@gmail.com>
2019-12-04 07:57:21 -0600

    work on CreateQuestionCheckboxForm

--- src/components/CreateQuestionCheckboxForm/CreateQuestionCheckboxForm.js
+++ src/components/CreateQuestionCheckboxForm/CreateQuestionCheckboxForm.js
@@ -4,55 +4,104 @@ import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import shortid from 'shortid';
+import './CreateQuestionCheckboxForm.css';
 
 class CreateQuestionCheckboxForm extends React.Component {
   state = {
     questionOptions: [
       {
-        id: '0',
+        id: shortid.generate(),
         name: '',
         answer: false,
       },
       {
-        id: '1',
+        id: shortid.generate(),
         name: '',
         answer: false,
       },
     ],
   };
 
-  handleOptionChange = (e) => {
+  handleOptionAnswerChange = (e) => {
     const newQuestionOptions = clonedeep(this.state.questionOptions);
-    newQuestionOptions.filter((option) => {
-      console.log(option.id, e.target.getAttribute('data-id'));
-      return option.id != e.target.getAttribute('data-id');
+    const option = this.state.questionOptions.find(
+      (opt) => opt.id === e.target.getAttribute('data-id'),
+    );
+    const editedOptions = newQuestionOptions.map((opt) => {
+      if (opt.id === option.id) {
+        opt.answer = !option.answer;
+      }
+      return opt;
     });
-    console.log(newQuestionOptions);
+    this.setState({
+      questionOptions: editedOptions,
+    });
   };
+
+  handleOptionNameChange = (e) => {
+    const newQuestionOptions = clonedeep(this.state.questionOptions);
+    const editedOptions = newQuestionOptions.map((option) => {
+      if (option.id === e.target.getAttribute('data-id')) {
+        option.name = e.target.value;
+      }
+      return option;
+    });
+    this.setState({
+      questionOptions: editedOptions,
+    });
+  };
+
+  handleOptionDelete = (e) => {
+    const newQuestionOptions = clonedeep(this.state.questionOptions);
+  };
+
+  handleOptionAdd = (e) => {};
 
   createQuestionInfoHandler = (e) => {
     e.preventDefault();
-    console.log(e.target.elements.answer[0].checked);
-    console.log(e.target.elements.answer[1].checked);
-    console.log(e.target.elements.option[0].value);
-    console.log(e.target.elements.option[1].value);
   };
 
   render() {
     return (
       <div className="CreateQuestionCheckboxForm">
         <Form onSubmit={this.createQuestionInfoHandler}>
-          {this.state.questionOptions.map((opt, i) => (
-            <InputGroup key={`option-${i}`}>
+          <Form.Label>Create Your Options:</Form.Label>
+          <p>
+            Add or delete as many question options as you would like. Check the
+            correct options.
+          </p>
+          {this.state.questionOptions.map((opt) => (
+            <InputGroup key={`option-${opt.id}`}>
               <InputGroup.Prepend>
-                <InputGroup.Radio name="answer" />
+                <InputGroup.Checkbox
+                  data-id={opt.id}
+                  onClick={this.handleOptionAnswerChange}
+                  name="answer"
+                />
               </InputGroup.Prepend>
               <FormControl
-                data-id="0"
-                onChange={this.handleOptionChange}
+                data-id={opt.id}
+                onChange={this.handleOptionNameChange}
                 value={opt.name}
                 name="option"
               />
+              <InputGroup.Append>
+                <Button
+                  variant="success"
+                  data-id={opt.id}
+                  onClick={this.handleOptionAdd}
+                >
+                  +
+                </Button>
+                <Button
+                  variant="danger"
+                  data-id={opt.id}
+                  onClick={this.handleOptionDelete}
+                >
+                  &times;
+                </Button>
+              </InputGroup.Append>
             </InputGroup>
           ))}
           <Button variant="info" type="submit">
