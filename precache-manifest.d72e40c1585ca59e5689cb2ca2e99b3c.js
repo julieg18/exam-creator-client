765a039ec3432877474fe58a4577d025c383e097
julieg18 <juliannagal18@gmail.com>
2020-01-29 12:21:31 -0600

    Updates

---
+++ precache-manifest.d72e40c1585ca59e5689cb2ca2e99b3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d5e1ac0d4f56f5833441f8196a3ca40",
+    "url": "/exam-creator/index.html"
+  },
+  {
+    "revision": "d8200cd608b83065c660",
+    "url": "/exam-creator/static/css/main.f1c7c5cc.chunk.css"
+  },
+  {
+    "revision": "db9538a34aa29157edab",
+    "url": "/exam-creator/static/js/2.77107798.chunk.js"
+  },
+  {
+    "revision": "d8200cd608b83065c660",
+    "url": "/exam-creator/static/js/main.ee2eb9e9.chunk.js"
+  },
+  {
+    "revision": "b905b93f2db7447f70ae",
+    "url": "/exam-creator/static/js/runtime-main.832c1d25.js"
+  }
+]);
